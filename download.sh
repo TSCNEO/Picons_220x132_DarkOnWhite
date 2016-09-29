f5cbe6bd1732083ff3c14550a1f5bbfa5bc54d4d
TSCNEO <tscneo@gmail.com>
2016-09-29 12:20:13 +0200

    Fix Script

--- download.sh
+++ download.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 cd /hdd/
+rm -R picon.old/
+mv picon/ picon.old/
 wget https://github.com/TSCNEO/Picons_220x132_DarkOnWhite/archive/master.zip
 unzip master.zip
 mv Picons_220x132_DarkOnWhite-master/ picon/
 rm master.zip
+chmod 777 picon/download.sh
