cd3cdc9af01b3e0f350ffc34950b1eafd626692e
TSCNEO <tscneo@gmail.com>
2016-10-03 08:47:26 +0200

    Updated Script

--- downloadDoW.sh
+++ downloadDoW.sh
@@ -6,5 +6,5 @@ wget https://github.com/TSCNEO/Picons_220x132_DarkOnWhite/archive/master.zip
 unzip master.zip
 mv Picons_220x132_DarkOnWhite-master/ picon/
 rm master.zip
-chmod 777 picon/download.sh
-mv picon/download.sh /usr/script/download.sh
+chmod 777 picon/downloadDoW.sh
+mv picon/downloadDoW.sh /usr/script/downloadDoW.sh
